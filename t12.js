0d4c9c889629309a71cd4a5746644f40cc33e333
chen <s9556010@gmail.com>
2023-04-26 15:06:51 +0800

    Update and rename t11.js to t12.js

--- t12.js
+++ t12.js
@@ -558,14 +558,14 @@ function redirect_main(){
     }
 }
 
-function vote_main(){
+function vote_main(hascapture){
     console.log("vote_main");
     if(!verify())return;
     if (window.location.href.match('https://www.stockvote.com.tw/evote/shareholder/00*')) {
         if(document.getElementsByClassName('c-main').length>0){
             var htmlContext =  document.getElementsByClassName('c-main')[0].innerHTML;
             if(htmlContext.indexOf("投票狀況")>0){
-                vote_displayUnvoteCount();
+                vote_displayUnvoteCount(hascapture);
             }else if(htmlContext.indexOf("行使表決權")>0){
                 vote_voteLaw();
             }else if(htmlContext.indexOf("候選人經歷")>0){
@@ -579,7 +579,7 @@ function vote_main(){
     }
 }
 
-function vote_displayUnvoteCount(){
+function vote_displayUnvoteCount(hascapture){
     console.log("vote_displayUnvoteCount");
     if(!verify())return;
     try{
@@ -599,6 +599,15 @@ function vote_displayUnvoteCount(){
 		var myhint = document.getElementsByClassName("c-main")[0];
 		if(unvote==0){
 			myhint.innerHTML = "<div class='pluginclass card '><div class='card-body'><svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' fill='currentColor' class='bi bi-check-circle' viewBox='0 0 16 16' style='color: green;'>  <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/>  <path d='M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z'/></svg> <font style='color: green; font-size:24px;'>\u5C1A\u672A\u6295\u7968:0 </font></div></div><br>"+myhint.innerHTML;
+			
+           		if(!hascapture){
+                		var indexInStorage = parseInt(localStorage.getItem(year+"#index"));
+                		var MaxindexInStorage = parseInt(localStorage.getItem(year+"#Maxindex"));
+
+                		localStorage.setItem(year+"#index",indexInStorage+1);
+                		document.pageForm.action = '/evote/logout.html';
+                		document.pageForm.submit();
+            		}			
 		}else{
 			myhint.innerHTML = "<div class='pluginclass card '><div class='card-body'><svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' fill='currentColor' class='bi bi-exclamation-triangle' viewBox='0 0 16 16' style='color: red;'>  <path d='M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z'/>  <path d='M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z'/></svg> <font style='color: red; font-size:24px;'>\u5C1A\u672A\u6295\u7968:"+unvote+"</font></div></div><br>"+myhint.innerHTML;
 			setTimeout(function(){eval(link); }, 3000);
